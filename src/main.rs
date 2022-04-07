004921019c799b693cce80fb034a9c91a5115d03
Vincent Savard <savard.vincent@gmail.com>
2022-04-06 20:20:33 -0400

    feat: introduce a simple command line interface for supported actions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+const ACTIONS: &[&str] = &["previous", "next"];
+
+#[derive(StructOpt)]
+#[structopt(name = "i3-tools", about = "Tools for the i3 window manager.")]
+struct Opt {
+    /// Sets the filename of the i3 socket for communication
+    #[structopt(short, long, parse(from_os_str), env = "I3SOCK")]
+    socket: PathBuf,
+
+    /// Performs the action
+    #[structopt(possible_values(ACTIONS))]
+    action: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt: Opt = Opt::from_args();
+    println!(
+        "Performing <{}> on <{}>",
+        opt.action,
+        opt.socket.to_str().unwrap()
+    );
 }
