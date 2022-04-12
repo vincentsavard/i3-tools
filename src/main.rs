e4c22db3ee32356fba414ed9b13c19288c18a1cc
Vincent Savard <savard.vincent@gmail.com>
2022-04-11 20:36:34 -0400

    chore: replace structopt with clap
    structopt is in maintenance mode now that clap includes the structopt features.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,26 @@
 extern crate core;
 
+use clap::Parser;
 use i3_tools::{FocusTarget, I3Service};
 use std::io::Error;
 use std::path::PathBuf;
-use structopt::StructOpt;
 
 const ACTIONS: &[&str] = &["previous", "next"];
 
-#[derive(StructOpt)]
-#[structopt(name = "i3-tools", about = "Tools for the i3 window manager.")]
+#[derive(Parser)]
+#[clap(name = "i3-tools", about = "Tools for the i3 window manager.")]
 struct Opt {
     /// Sets the filename of the i3 socket for communication
-    #[structopt(short, long, parse(from_os_str), env = "I3SOCK")]
+    #[clap(short, long, parse(from_os_str), env = "I3SOCK")]
     socket: PathBuf,
 
     /// Performs the action
-    #[structopt(possible_values(ACTIONS))]
+    #[clap(possible_values(ACTIONS))]
     action: String,
 }
 
 fn main() -> Result<(), Error> {
-    let opt: Opt = Opt::from_args();
+    let opt: Opt = Opt::parse();
     let mut i3service = I3Service::connect(opt.socket)?;
     let target = match opt.action.as_str() {
         "previous" => FocusTarget::Previous,
